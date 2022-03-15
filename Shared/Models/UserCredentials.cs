5d574e53d210d3128d2df27f50a55408ffe0427f
RustamAhmadov <ahmadov.rustam114@gmail.com>
2022-03-15 15:46:25 +0400

    Add UserCredentials class

---
+++ Shared/Models/UserCredentials.cs
@@ -0,0 +1,17 @@
+
+
+
+namespace Server.Models
+{
+    public class UserCredentials
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public string Password{get;set;}
+        public UserCredentials(in string name,in string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
