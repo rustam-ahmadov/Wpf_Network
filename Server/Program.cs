c04fb7b1d57146bda9ce1a7d2b35134b2e4b4903
RustamAhmadov <ahmadov.rustam114@gmail.com>
2022-03-15 15:12:46 +0400

    Add server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
+        private const int PORT = 8080;
+        private const string IPADD = "127.0.0.1";
+        private static Socket socket ;
+        private static IPAddress? ip;
+
+        private static IPEndPoint? endPoint;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            socket  = new(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            ip = IPAddress.Parse(IPADD);
+            endPoint = new(ip,PORT);
+            
+
+
+
+        }
+
+        private static async Task StartServer()
+        {
+            socket.Bind(endPoint);
+            socket.Listen();
+
+
+
+            while(true)
+            {
+               
+            }
         }
     }
 }
