32836643d3c0dfb0fb1e4e2a8791177c3f3d9ec3
RustamAhmadov <ahmadov.rustam114@gmail.com>
2022-03-18 01:18:02 +0400

    Add userRequest

--- Server/Program.cs
+++ Server/Program.cs
@@ -13,20 +13,18 @@ namespace Server
     {
         private const int PORT = 8080;
         private const string IPADD = "127.0.0.1";
-        private static Socket socket ;
+        private static Socket socket;
         private static IPAddress? ip;
 
         private static IPEndPoint? endPoint;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
-            socket  = new(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             ip = IPAddress.Parse(IPADD);
-            endPoint = new(ip,PORT);
-            
+            endPoint = new(ip, PORT);
 
-
-
+            await StartServer();
         }
 
         private static async Task StartServer()
@@ -35,25 +33,24 @@ namespace Server
             socket.Listen();
 
 
-
-            while(true)
+            while (true)
             {
-               Socket client = await socket.AcceptAsync();
-               System.Console.WriteLine("Client connected!");
+                Socket client = await socket.AcceptAsync();
+                System.Console.WriteLine("Client connected!");
 
-               ThreadPool.QueueUserWorkItem(async obj=>
-               {
-                   while(true)
-                   {
-                       byte[] buffer = new byte[65000];
-                       int size = await client.ReceiveAsync(buffer,SocketFlags.None);
+                ThreadPool.QueueUserWorkItem(async obj =>
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[65000];
+                        int size = await client.ReceiveAsync(buffer, SocketFlags.None);
 
-                       string data = Encoding.UTF8.GetString(buffer,0,size);
-                       Request userRequest = JsonSerializer.Deserialize<Request>(data);
-                       
-                       System.Console.WriteLine( data);
-                   }
-               });
+                        string data = Encoding.UTF8.GetString(buffer, 0, size);
+                        Request userRequest = JsonSerializer.Deserialize<Request>(data);
+
+                        System.Console.WriteLine(userRequest.Credentials.Name);
+                    }
+                });
             }
         }
     }
